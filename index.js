8c034db64b3bc156768af39064185ff28fd3e81c
Rajat-295 <rajat@arviussoft.com>
2025-10-30 18:34:54 +0530

    add

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const server = http.createServer(app);
 const sql = require('mssql');
 const bodyParser = require('body-parser');
 const PORT =  process.env.PORT || 8080;
-
 app.use(bodyParser.json());
+
 
 app.get('/api/getcallwebhook', async (req, res) => {
 
@@ -27,6 +27,9 @@ app.get('/api/getcallwebhook', async (req, res) => {
         await sql.close();
     }
 });
+
+
+
 
 app.post('/api/getcallwebhook', async (req, res) => {
 
@@ -64,6 +67,46 @@ app.post('/api/getcallwebhook', async (req, res) => {
         await sql.close();
     }
 });
+
+
+
+
+
+app.post('/api/getagentstatus', async (req, res) => {
+
+    try {
+        const pool = await poolPromise;
+        console.log('Connected to the database.');
+        const request = pool.request();
+
+        // Input: JSON payload
+        request.input('json', sql.NVarChar(sql.MAX), JSON.stringify(req.body));
+        // Output: ERROR message (VARCHAR(1000))
+        request.output('ERROR', sql.VarChar(1000));
+        // Execute the stored procedure
+        const result = await request.execute('usp_InsertjsonDataToAgentsActivity');
+
+        console.log('SQL procedure executed successfully.');
+        console.log('Output parameter (ERROR):', result.output.ERROR);
+
+        res.json({
+            success: result.output.ERROR === 'SUCCESS',
+            message: result.output.ERROR,
+            data: req.body
+        });
+
+    } catch (error) {
+        console.error('Error executing SQL query:', error);
+        res.status(500).json({ error: 'Internal Server Error', details: error.message });
+    } finally {
+        console.log('Closing the database connection.');
+        await sql.close();
+    }
+});
+
+
+
+
 
 
 
